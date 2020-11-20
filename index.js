2e8aed24f42b60d3d9410a23f726c9e890d9bf80
wtwerner <wtwerner@gmail.com>
2020-11-20 09:09:04 -0600

    commit tree ee0c052052fe2ef1fd6278bc8abc3ac862d2980c

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-const testVar = {}
+const record = [
+  {year: "2018", result: "L"},
+  {year: "2017", result: "W"},
+  {year: "2016", result: "N/A"}
+]
 
-function testFunc() {
-  return "hi"
+function superbowlWin(record) {
+  let result = record.find(record => record.result === "W")
+  if(result) {
+    return result.year;
+  } else {
+    return undefined;
+  }
 }
